ca35df70b3e9e19a8ad91730de251797b9090ab7
Fabio Rizzuto <fbrizzuto@gmail.com>
2019-01-19 23:54:58 +0100

    Changed project name, added a delay in main function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <thread>
 
 int main() {
+    std::cout << "Starting...." << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     std::cout << "Pippo" << std::endl;
     return 0;
 }
